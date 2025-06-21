93e6f92f1ec9852494c3f6a69bf467d11c795fa3
Hien Luong Tran Hy <hyhien@gmail.com>
2025-06-21 08:13:55 +0700

    BT01 - Lab03 async

---
+++ Buoi04/Lab03/Controllers/DemoController.cs
@@ -0,0 +1,34 @@
+﻿using Lab03.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab03.Controllers
+{
+    public class DemoController : Controller
+    {
+        public async Task<IActionResult> AsyncDemo()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var x = MyDemo.FuncAAsync();
+            var y = MyDemo.FuncBAsync();
+            var z = MyDemo.FuncCAsync();
+            await x; await y; await z;
+            sw.Stop();
+
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+
+        public IActionResult SyncDemo()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            MyDemo.FuncA();
+            MyDemo.FuncB();
+            MyDemo.FuncC();
+            sw.Stop();
+
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
