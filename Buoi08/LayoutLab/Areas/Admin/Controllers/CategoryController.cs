c479870b981ca22eeab1dd51c8c1677ec2b1ae41
Hien Luong Tran Hy <hyhien@gmail.com>
2025-07-05 09:31:28 +0700

    Demo Area

---
+++ Buoi08/LayoutLab/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutLab.Areas.Admin.Controllers
+{
+	[Area("admin")]
+	public class CategoryController : Controller
+	{
+		// host/admin/Category/Index
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
